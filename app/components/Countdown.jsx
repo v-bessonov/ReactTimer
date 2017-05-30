6cf786edbdf3ffd07b19cf462ba722d185970583
vladdnc <vladdnc@mail.ru>
2017-05-30 15:57:38 +0300

    Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function() {
     return(
           <div>
-            <h1 className="text-center page-title"> Countdown </h1>
-
+            <Clock totalSeconds={129} />
           </div>
     );
   }
