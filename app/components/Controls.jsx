6db6f9990565c07797ebf932f2d3db8e93c8acfc
vladdnc <vladdnc@mail.ru>
2017-06-05 10:49:49 +0300

    Wire up controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -3,7 +3,8 @@ var React = require('react');
 
 var Controls = React.createClass({
   propsTypes : {
-    countdownStatus : React.PropTypes.string.isRequired
+    countdownStatus : React.PropTypes.string.isRequired,
+    onStatusChange : React.PropTypes.func.isRequired
   },
   // getInitialState: function(){
   //   return {
@@ -30,26 +31,25 @@ var Controls = React.createClass({
   //     });
   //   },1000);
   // },
-  // handleSetCountdown: function(seconds){
-  //   this.setState({
-  //     count:seconds,
-  //     countdownStatus: 'started'
-  //   });
-  // },
+  onStatusChange: function(newStatus){
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
+  },
 
   render: function() {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if(countdownStatus === 'started'){
-        return <button className="button secondary">Pause</button>
+        return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
       } else if(countdownStatus === 'paused'){
-        return <button className="button primary">Start</button>
+        return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
     return(
           <div className="controls">
             {renderStartStopButton()}
-            <button className="button alert hollow">Clear</button>
+            <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
           </div>
     );
   }
